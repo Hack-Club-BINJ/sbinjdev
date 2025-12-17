d6ece3e7dd63d75f9f6fd0fc08cad4e4f0735525
david-why <david_why@outlook.com>
2025-12-17 23:13:50 +0800

    allow dashes

--- src/schemas.ts
+++ src/schemas.ts
@@ -6,7 +6,7 @@ export const CreateShortcutBody = z.object({
     .string()
     .describe('The suggested slug for the shortcut')
     .min(2)
-    .regex(/^[a-zA-Z0-9]+$/, 'Slug must contain only alnum characters')
+    .regex(/^[a-zA-Z0-9-]+$/, 'Slug must contain only alnum characters and dashes')
     .refine((value) => !['api'].includes(value), 'Slug is reserved')
     .optional(),
 })
